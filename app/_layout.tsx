d850ea78ba2278f5a0dd0944c30319bd3f5085b4
HassanShakoor <hassanshakoor308@gmail.com>
2025-04-27 17:14:24 +0500

    fix: fix ios font issue

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,16 +3,26 @@ import "../global.css";
 import { ThemeProvider } from "@/app/contexts/useThemeContext";
 import { StatusBar } from "expo-status-bar";
 import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
+  const [loaded, error] = useFonts({
     ApfelGrotezk: require("@/assets/fonts/ApfelGrotezk.otf"),
     Brockmann: require("@/assets/fonts/Brockmann.ttf"),
     Inter: require("@/assets/fonts/Inter.ttf"),
   });
 
-  if (!loaded) {
-    console.log("Loading layout...");
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
   }
 
   return (
