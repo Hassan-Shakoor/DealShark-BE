965ecfc50091ce8da7e8e6c5e87705904ebed86b
HassanShakoor <hassanshakoor308@gmail.com>
2025-05-30 13:17:25 +0500

    feat: hide change avatar button on companion setting

--- app/components/companion-setting/CompanionInfo.tsx
+++ app/components/companion-setting/CompanionInfo.tsx
@@ -23,23 +23,23 @@ export const CompanionInfo: FunctionComponent = () => {
 
   return (
     <View className={"mt-2 flex flex-col gap-1"}>
-      <TouchableOpacity
-        activeOpacity={0.7}
-        className={
-          "flex flex-row items-center justify-between rounded-xl bg-white/50 px-4.5 py-5.5 dark:bg-dark-septenary/50"
-        }
-      >
-        <Text
-          className={"font-apfelGrotezk text-lg text-black dark:text-white"}
-        >
-          Change Avatar
-        </Text>
-        <AntDesign
-          name="right"
-          size={14}
-          color={colorScheme === Theme.Dark ? "#CDC3C3" : "#000000"}
-        />
-      </TouchableOpacity>
+      {/*<TouchableOpacity*/}
+      {/*  activeOpacity={0.7}*/}
+      {/*  className={*/}
+      {/*    "flex flex-row items-center justify-between rounded-xl bg-white/50 px-4.5 py-5.5 dark:bg-dark-septenary/50"*/}
+      {/*  }*/}
+      {/*>*/}
+      {/*  <Text*/}
+      {/*    className={"font-apfelGrotezk text-lg text-black dark:text-white"}*/}
+      {/*  >*/}
+      {/*    Change Avatar*/}
+      {/*  </Text>*/}
+      {/*  <AntDesign*/}
+      {/*    name="right"*/}
+      {/*    size={14}*/}
+      {/*    color={colorScheme === Theme.Dark ? "#CDC3C3" : "#000000"}*/}
+      {/*  />*/}
+      {/*</TouchableOpacity>*/}
       <TouchableOpacity
         onPress={handleToggleGender}
         activeOpacity={0.7}
