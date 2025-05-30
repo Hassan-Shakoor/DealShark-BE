f41d7023238277b0a62a05c5508b967f6eaf441f
HassanShakoor <hassanshakoor308@gmail.com>
2025-05-31 00:00:44 +0500

    fix: fix keyboard avoiding issue on chat screen

--- app/(protected)/(chat)/[id].tsx
+++ app/(protected)/(chat)/[id].tsx
@@ -3,14 +3,21 @@ import { CustomSafeArea } from "@/app/components/ui/CustomSafeArea";
 import { ChatDetailHeader } from "@/app/components/chat/chat-detail/ChatDetailHeader";
 import { MessageList } from "@/app/components/chat/chat-detail/MessageList";
 import { KeyboardSection } from "@/app/components/chat/chat-detail/KeyboardSection";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
 const ChatView = () => {
   return (
     <ChatBackground>
-      <CustomSafeArea classNames={"flex flex-col gap-3.5"}>
+      <CustomSafeArea classNames={"flex flex-col"}>
         <ChatDetailHeader />
-        <MessageList />
-        <KeyboardSection />
+        <KeyboardAvoidingView
+          behavior={Platform.OS === "ios" ? "padding" : "height"}
+          style={{ flex: 1 }}
+          keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 20}
+        >
+          <MessageList />
+          <KeyboardSection />
+        </KeyboardAvoidingView>
       </CustomSafeArea>
     </ChatBackground>
   );
--- app/components/chat/chat-detail/KeyboardSection.tsx
+++ app/components/chat/chat-detail/KeyboardSection.tsx
@@ -121,11 +121,7 @@ export const KeyboardSection: FunctionComponent = () => {
       {/*>*/}
       {/*  {`Free trial: ${FreeAllowedMessageCount - individualMessageCount} Messages left`}*/}
       {/*</Text>*/}
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "height"}
-        keyboardVerticalOffset={100}
-        className="mb-4 px-4"
-      >
+      <View className={"mb-4 px-4"}>
         <View className="mb-4 flex-row items-center rounded-full border border-blue-secondary bg-white p-1 dark:bg-dark-senary">
           <TextInput
             className="min-h-12 flex-1 px-3 py-4 font-inter text-base text-black dark:text-white"
@@ -145,7 +141,7 @@ export const KeyboardSection: FunctionComponent = () => {
             <Ionicons name="send" size={20} color="white" />
           </TouchableOpacity>
         </View>
-      </KeyboardAvoidingView>
+      </View>
     </View>
   );
 };
